b946f1d2a591f54715a40c4d55b653c680512609
superlou <lousimons@gmail.com>
2025-03-23 19:56:45 -0400

    Added bootstrap

--- app/components/glass-builder.js
+++ app/components/glass-builder.js
@@ -2,15 +2,15 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class GlassBuilder extends Component {
-    @tracked diameter = 15.0;
-    @tracked rect_w = 5;
-    @tracked rect_h = 1.5;
-    @tracked rect_count = 8;
+  @tracked diameter = 15.0;
+  @tracked rect_w = 5;
+  @tracked rect_h = 1.5;
+  @tracked rect_count = 8;
 
-    get inset_rect_offset() {
-        let radius = this.diameter / 2.0;
-        console.log(this.rect_h);
-        let h_i = Math.sqrt(radius ** 2 - (this.rect_w / 2.0) ** 2);
-        return h_i - this.rect_h / 2.0;
-    }
+  get inset_rect_offset() {
+    let radius = this.diameter / 2.0;
+    console.log(this.rect_h);
+    let h_i = Math.sqrt(radius ** 2 - (this.rect_w / 2.0) ** 2);
+    return h_i - this.rect_h / 2.0;
+  }
 }
